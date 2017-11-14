756b3143ddbd437348bdf395e97c616b1c1a088a
Adam Bohannon <abo46n2@gmail.com>
2017-11-14 13:46:24 -0800

    Created map

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* global d3:true */
 /* eslint no-undef: "error" */
-const MAP_DATA = 'https://d3js.org/world-50m.v1.json'
+const MAP_DATA = 'https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json'
 const METEORITE_DATA = 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/meteorite-strike-data.json'
 
 class Chart {
@@ -37,10 +37,10 @@ class Chart {
 
   // render the data
   renderData (data) {
-    const topology = data[0]
+    const map = data[0]
     const features = data[1]
 
-    console.log(topology)
+    console.log(map)
     console.log(features)
 
     // chart dimensions
@@ -49,12 +49,24 @@ class Chart {
     const padding = 24
 
     // create svg canvas
-
     const svg = d3.select('svg')
       .attr('width', width)
       .attr('height', height)
       .style('padding', padding)
       .style('background-color', 'lightgrey')
+
+    const projection = d3.geoOrthographic()
+
+    const path = d3.geoPath()
+      .projection(projection)
+
+    // Join the FeatureCollection's features array to path elements
+    const u = d3.select('svg')
+      .selectAll('path')
+      .data(map.features)
+      .enter()
+      .append('path')
+      .attr('d', path)
   }
   // initialize the chart by executing fetch
   initialize () {
